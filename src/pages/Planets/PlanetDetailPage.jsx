f2d4249c2c339b0b50f2a1ac266b5abada31288e
Ajit <ajitlouriyam59@gmail.com>
2023-12-27 08:37:24 +0530

    planet Detail Page

---
+++ src/pages/Planets/PlanetDetailPage.jsx
@@ -0,0 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
+export function PlanetListDetailPage(){
+    const { id = '1'} = useParams();
+    const { data, isLoading, error, isError } = useQuery({
+        queryKey: ['planet/details', id],
+        queryFn: async() => {
+            const res = await fetch(`https://swapi.dev/api/planets/${id}`);
+            if (res.ok) {
+                return res.json();
+            }
+
+            return Promise.reject('Could not fetch data');
+        }
+    });
+
+    if(isLoading){
+        return (
+            <div 
+            style={{
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}
+            >
+                <span className="loader"></span>
+            </div>
+        );
+    }
+    if (isError) {
+        return (
+            <div 
+                style={{
+                    height: '100',
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+                >
+                    <p>{String(error)}</p>
+                </div>
+        );
+    }
+
+    return(
+        <div>
+            <h1>Planets Details</h1>
+            <h3>{data.name}</h3>
+            <p>Diameter: {data.diameter}</p>
+            <p>Rotation_period: {data.rotation_period}</p>
+
+        </div>
+    );
+}
